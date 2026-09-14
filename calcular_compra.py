0034326201f5cd008fc7494363b5a6141660778c
CaChilam <ca.chilam@uea.edu.ec>
2026-09-13 19:09:10 -0500

    Add files via upload

---
+++ calcular_compra.py
@@ -0,0 +1,9 @@
+def calcular_total(precio, cantidad):
+    total = precio * cantidad
+    return total
+
+# Uso de la función
+precio = 10
+cantidad = 3
+resultado = calcular_total(precio, cantidad)
+print(resultado)
